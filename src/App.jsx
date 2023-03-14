3cb2bcc5808fefa846e3cb00f92125ecdce5236f
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-14 18:03:19 +0530

    ci: add content in Feed page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+/* Pages */
+import Feed from "./pages/Feed";
+import VideoDetail from "./pages/VideoDetail";
+import ChannelDetail from "./pages/ChannelDetail";
+import SearchFeed from "./pages/SearchFeed";
+
+/* Components */
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
-    <React.Fragment>
-      <h1>Good Morning BIT</h1>
-    </React.Fragment>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#222" }}>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTern" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,16 +1,41 @@
-import { Box, Typography } from "@mui/material";
+// import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import SideBar from "../components/SideBar";
+import Videos from "../components/Videos";
 
 export default function Feed() {
   return (
-    <Box component="main" sx={{ backgroundColor: "#898" }}>
-      <Typography
-        component="h1"
-        variant="overline"
-        fontFamily="inherit"
-        fontWeight="bolder"
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "88vh" },
+          borderRight: "1px solid white",
+          px: { sm: 0, md: 2 }
+        }}
       >
-        Good Morning BIT
-      </Typography>
-    </Box>
+        <SideBar />
+        <Typography
+          variant="body2"
+          color="rgb(255,255,255)"
+          fontFamily="inherit"
+          fontSize="1.1rem"
+          mt={1}
+        >
+          Copyright {new Date().getFullYear()} Rahulbaran
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ height: "90vh", overflowY: "auto", flex: 2 }}>
+        <Typography
+          variant="h4"
+          sx={{ fontWeight: "bold", mb: "1em", color: "white" }}
+        >
+          New <span style={{ color: "red" }}>videos</span>
+        </Typography>
+
+        <Videos />
+      </Box>
+    </Stack>
   );
 }
